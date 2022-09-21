dfd18ea117e3eb0ed77047a802741fa12c7ff3af
Rafaelleo93 <rafaelleonardo93@gmail.com>
2022-09-21 16:21:21 +0000

    ejercicio 2 completado

---
+++ 02-listas/info.js
@@ -0,0 +1,14 @@
+const miInformacion = {
+    nombre: "Rafael Leonardo",
+    edad: 28,
+    desarrollador: true,
+    fechaDeNacimiento: new Date("october 1, 1993"),
+    libroFavorito: {
+        titulo: "El código Da Vinci",
+        autor: "Dan Brown",
+        fecha: new Date("march 18, 2003"),
+        url: "https://es.wikipedia.org/wiki/El_c%C3%B3digo_Da_Vinci"
+    }
+}
+
+console.log(miInformacion)
